33b36652bc3f437c621aed7a2f65ed92aacc6d3d
seanflyyy <seanerwenhan@gmail.com>
2023-04-21 12:29:36 +0800

    Make minor adjustments to UI

--- src/components/general/SideMenu.tsx
+++ src/components/general/SideMenu.tsx
@@ -5,52 +5,52 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 export default function SideMenu() {
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.clerkie}>
-        <Image
-          src={"/general/sideMenu/ClerkieIcon.svg"}
-          alt={`Clerkie logo`}
-          width={24}
-          height={24}
-        />
-        <div>Clerkie Challenge</div>
-      </div>
-      {
-        [
-          {
-            name: 'Home',
-            link: '/home',
-            icon: 'HomeIcon.svg',
-          },
-          {
-            name: 'Friends',
-            link: '/friends',
-            icon: 'FriendsIcon.svg',
-          }
-        ]
-          .map((item, index) => (
-            <Link href={item.link} key={index}>
-              <div
-                className={router.pathname == item.link
-                  ? styles.sidePanelRowSelected
-                  : styles.sidePanelRow
-                }
-              >
-                {/* <div className={styles.icon}>{item.icon}</div> */}
+    return (
+        <div className={styles.sideMenu}>
+            <div className={styles.clerkie}>
                 <Image
-                  src={"/general/sideMenu/" + item.icon}
-                  alt={`${item.name} logo`}
-                  width={24}
-                  height={24}
+                    src={"/general/sideMenu/ClerkieIcon.svg"}
+                    alt={`Clerkie logo`}
+                    width={24}
+                    height={24}
                 />
-                <div className={styles.name}>{item.name}</div>
-              </div>
-            </Link>
-          ))
-      }
-    </div>
-  )
+                <div>Clerkie Challenge</div>
+            </div>
+            {
+                [
+                    {
+                        name: 'Home',
+                        link: '/home',
+                        icon: 'HomeIcon.svg',
+                    },
+                    {
+                        name: 'Friends',
+                        link: '/friends',
+                        icon: 'FriendsIcon.svg',
+                    }
+                ]
+                    .map((item, index) => (
+                        <Link href={item.link} key={index}>
+                            <div
+                                className={router.pathname == item.link
+                                    ? styles.sidePanelRowSelected
+                                    : styles.sidePanelRow
+                                }
+                            >
+                                {/* <div className={styles.icon}>{item.icon}</div> */}
+                                <Image
+                                    src={"/general/sideMenu/" + item.icon}
+                                    alt={`${item.name} logo`}
+                                    width={24}
+                                    height={24}
+                                />
+                                <div className={styles.name}>{item.name}</div>
+                            </div>
+                        </Link>
+                    ))
+            }
+        </div>
+    )
 }
